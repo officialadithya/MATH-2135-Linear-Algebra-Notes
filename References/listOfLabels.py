b332a32493bb64bc012e7ab60ca5fa88de60cbc0
Adithya Bhaskara <adithya.bhaskara@colorado.edu>
2022-12-31 05:57:47 -0700

    List of Theorems and Definitions Instead of All Labels

--- References/listOfLabels.py
+++ References/listOfLabels.py
@@ -16,8 +16,6 @@ for i, _ in enumerate(lines):
     title["appendix"] = "\\textbf{Appendix}"
     title["def"] = "\\textit{Definition}"
     title["thm"] = "\\textit{Theorem}"
-    title["exa"] = "\\textit{Example}"
-    title["exe"] = "\\textit{Exercise}"
     
     reference = lines[i][lines[i].find("{")+1:lines[i].find("}")]
     lines[i] = title[reference[0:reference.find(":")]]+" \\ref{"+reference+"}, Page \\pageref{"+reference+"} \\\\"
