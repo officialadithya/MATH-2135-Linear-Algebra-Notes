597522e64790cc3f4fcee91a195b7db4a149180b
Adithya Bhaskara <adithya.bhaskara@colorado.edu>
2022-12-31 23:24:22 -0700

    Modified list of labels to include theorem and definition names

--- References/listOfLabels.py
+++ References/listOfLabels.py
@@ -4,7 +4,7 @@ lines = []
 with open("main.aux") as f:
 
   lines = [i.strip() for i in f.readlines()]
-  
+
 for i, _ in enumerate(lines):
 
   if "\\newlabel" not in lines[i]: lines[i] = ""
@@ -14,11 +14,19 @@ for i, _ in enumerate(lines):
     title = defaultdict(str)
     title["chapter"] = "\\textbf{Chapter}"
     title["appendix"] = "\\textbf{Appendix}"
-    title["def"] = "\\textit{Definition}"
-    title["thm"] = "\\textit{Theorem}"
+    title["def"] = "\\textsc{Definition}"
+    title["thm"] = "\\textsc{Theorem}"
     
     reference = lines[i][lines[i].find("{")+1:lines[i].find("}")]
-    lines[i] = title[reference[0:reference.find(":")]]+" \\ref{"+reference+"}, Page \\pageref{"+reference+"} \\\\"
+    header = title[reference[0:reference.find(":")]]
+    line = header+" \\ref{"+reference+"}, \\textsc{Page} \\pageref{"+reference+"}"
+    
+    if not (header == "\\textbf{Chapter}" or header == "\\textbf{Appendix}"):
+      lines[i] = line+" \\textit{"+lines[i][lines[i].find("\\,\\,")+4:lines[i].find("tcb")-2]+"}"
+      
+    else: lines[i] = line
+      
+    lines[i] += " \\\\"
     
 nonempty = [i for i in lines if i != "" and i[0] != " "]
 print("\n".join(nonempty))
